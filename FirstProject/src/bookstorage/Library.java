38dc22cd7d2aa19f1c1a54ae155e1d804c7a2055
Amwaj <amwajsabri@gmail.com>
2025-02-11 12:13:31 -0600

    Added Library attributes

--- FirstProject/src/bookstorage/Library.java
+++ FirstProject/src/bookstorage/Library.java
@@ -1,5 +1,12 @@
 package bookstorage;
 
 public class Library {
-
+	private Book[] books;
+	private int count;
+	
+	// Initalizes the Book array and count
+	public Library() {
+		books = new Book[5];
+		count = 0;
+	}
 }
